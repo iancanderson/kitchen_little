eb09ea8d5694b4c9d039d201dee56c14ecfaa90d
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-01-23 12:21:05 -0500

    green - dynamic titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
-
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it 'should have the right title' do
+      get 'home'
+      response.should have_selector("title", :content => "Kitchen Little | Home")
     end
   end
 
@@ -13,6 +19,11 @@ describe PagesController do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it 'should have the right title' do
+      get 'about'
+      response.should have_selector("title", :content => "Kitchen Little | About")
     end
   end
 
