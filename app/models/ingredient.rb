425648a4871c4d0e8e42ba6e6c11bc213c3982c7
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-01-24 20:00:48 -0500

    ingredient model validations, tests

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, :length => { :minimum => 1}
+  validates :name, :presence => true, :length => { :maximum => 50 }
+  validates :user_id, :presence => true
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Ingredient do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = Factory(:user)
+    @attr = { :name => "garlic" }
+  end
+  
+  it 'should create a new instance with valid attributes' do
+    @user.ingredients.create!(@attr)
+  end
+  
+  describe 'user association' do
+  
+    before(:each) do
+      @ingredient = @user.ingredients.create(@attr)
+    end
+    
+    it 'should have a user attribute' do
+      @ingredient.should respond_to(:user)
+    end
+    
+    it 'should have the right associated user' do
+      @ingredient.user_id.should == @user.id
+      @ingredient.user.should == @user
+    end
+  end
+
+  describe 'validations' do
+    
+    it 'should have a user id' do
+      Ingredient.new(@attr).should_not be_valid
+    end
+    
+    it 'should require a nonblank name' do
+      @user.ingredients.build(:name => "   ").should_not be_valid
+    end
+    
+    it 'should reject a long name' do
+      @user.ingredients.build(:name => 'a'*51).should_not be_valid
+    end
+  end
 end
