3f3a9d4e2737c0b4d259fdb219c1fc2cda510de8
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-01-25 00:18:39 -0500

    sign up button on home page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,6 +13,27 @@ describe PagesController do
       get 'home'
       response.should have_selector("title", :content => "Kitchen Little | Home")
     end
+    
+    describe "when not signed in" do
+      it "should show the signup button" do
+        get 'home'
+        response.should have_selector("a",  :content => "Sign up now!",
+                                            :class => "signup_button round")
+      end
+    end
+    
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = Factory(:user)
+        sign_in @user
+      end
+      
+      it "should not show the signup button'" do
+        get :home
+        response.should_not have_selector("a",  :content => "Sign up now!")
+      end
+    end
   end
 
   describe "GET 'about'" do
