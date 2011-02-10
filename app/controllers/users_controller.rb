09c132547bc6d696956f04308f0cea61b03f6d86
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-02-10 17:17:16 -0500

    Better titles for pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  
+  include UsersHelper
   def show
     @user = User.find(params[:id])
+    @title = "#{possessive_pronoun_for_user} Kitchen"
   end
 end
