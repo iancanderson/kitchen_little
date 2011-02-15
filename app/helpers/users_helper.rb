168e3ed0aab1090423c297618d48ddf5b8ba3224
Ian C. Anderson <anderson.ian.c@gmail.com>
2011-02-14 23:01:02 -0500

    215627-10006485: Add links to find recipes using your ingredients

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,12 @@ module UsersHelper
   def possessive_pronoun_for_user
     current_user == @user ? "My" : "#{@user.username}'s"
   end
+
+  def recipe_puppy_link(user)
+    comma_separated_ingredients = ""
+    user.ingredients.each do |ingredient|
+      comma_separated_ingredients += ingredient.name + ","
+    end
+    "http://www.recipepuppy.com/?i=#{comma_separated_ingredients}"
+  end
 end
